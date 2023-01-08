ead995d4e2b4c1697d9a3465ac62b53eb930eb8a
Devrath <devrath.dev595@gmail.com>
2023-01-08 12:25:56 +0530

    image added

---
+++ Application/app/src/main/java/com/demo/code/manualencoding/Address.kt
@@ -0,0 +1,3 @@
+package com.demo.code.manualencoding
+
+data class Address (val address: String,val pinCode:Int)
---
+++ Application/app/src/main/java/com/demo/code/manualencoding/StudentInfo.kt
@@ -0,0 +1,24 @@
+package com.demo.code.manualencoding
+
+import android.os.Build
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.Transient
+import kotlinx.serialization.json.Json
+
+@Serializable
+data class StudentInfo(
+    @SerialName("name")
+    val studentName: String,
+    @SerialName("age")
+    val studentAge: Int,
+    val address: Address
+)
+
+fun main() {
+    val lollipop = StudentInfo("Ramesh", 21)
+    println(lollipop)
+
+    val json = Json.encodeToString(StudentInfo.serializer(), lollipop)
+    println(json)
+}
