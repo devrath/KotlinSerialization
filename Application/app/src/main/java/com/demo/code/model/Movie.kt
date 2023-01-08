f89e531e3f54fc718311e6f4eddfd96da6bb5e8e
Devrath <devrath.dev595@gmail.com>
2023-01-08 10:52:47 +0530

    project added

--- Application/app/src/main/java/com/demo/code/model/Movie.kt
+++ Application/app/src/main/java/com/demo/code/model/Movie.kt
@@ -1,3 +1,6 @@
 package com.demo.code.model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class Movie(val id: Int, val poster: String)
--- Application/app/src/main/java/com/demo/code/ui/MovieListFragment.kt
+++ Application/app/src/main/java/com/demo/code/ui/MovieListFragment.kt
@@ -14,69 +14,75 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class MovieListFragment: Fragment() {
+class MovieListFragment : Fragment() {
 
-  private lateinit var movieAdapter: MovieAdapter
+    private lateinit var movieAdapter: MovieAdapter
 
-  private var _binding: FragmentMovieListBinding? = null
-  private val binding get() = _binding!!
+    private var _binding: FragmentMovieListBinding? = null
+    private val binding get() = _binding!!
 
-  override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-    _binding = FragmentMovieListBinding.inflate(inflater, container, false)
-    return binding.root
-  }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMovieListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
-  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-    super.onViewCreated(view, savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-    movieAdapter = MovieAdapter()
-    binding.movieRecyclerview.adapter = movieAdapter
+        movieAdapter = MovieAdapter()
+        binding.movieRecyclerview.adapter = movieAdapter
 
-    val movieApi = ApiProvider.getMovieApi(requireContext())
-    movieApi.movies().enqueue(object : Callback<List<Movie>> {
-      override fun onFailure(call: Call<List<Movie>>, t: Throwable) {
-        showErrorState()
-      }
+        val movieApi = ApiProvider.getMovieApi(requireContext())
+        movieApi.movies().enqueue(object : Callback<List<Movie>> {
+            override fun onFailure(call: Call<List<Movie>>, t: Throwable) {
+                showErrorState()
+            }
 
-      override fun onResponse(call: Call<List<Movie>>,
-                              response: Response<List<Movie>>) {
-        if (response.isSuccessful && response.body() != null) {
-          val movies = response.body()!!
-          if (movies.isEmpty()) {
-            showEmptyDataState()
-          } else {
-            showMovieList(movies)
-          }
-        } else {
-          showErrorState()
-        }
-      }
-    })
-  }
+            override fun onResponse(
+                call: Call<List<Movie>>,
+                response: Response<List<Movie>>
+            ) {
+                if (response.isSuccessful && response.body() != null) {
+                    val movies = response.body()!!
+                    if (movies.isEmpty()) {
+                        showEmptyDataState()
+                    } else {
+                        showMovieList(movies)
+                    }
+                } else {
+                    showErrorState()
+                }
+            }
+        })
+    }
 
-  private fun showEmptyDataState() {
-    binding.movieRecyclerview.visibility = View.GONE
-    binding.progressBar.visibility = View.GONE
-    binding.textview.visibility = View.VISIBLE
-    binding.textview.text = getString(R.string.there_seems_to_be_no_data)
-  }
+    private fun showEmptyDataState() {
+        binding.movieRecyclerview.visibility = View.GONE
+        binding.progressBar.visibility = View.GONE
+        binding.textview.visibility = View.VISIBLE
+        binding.textview.text = getString(R.string.there_seems_to_be_no_data)
+    }
 
-  private fun showMovieList(movies: List<Movie>) {
-    binding.movieRecyclerview.visibility = View.VISIBLE
-    binding.progressBar.visibility = View.GONE
-    binding.textview.visibility = View.GONE
-    movieAdapter.setCharacterList(movies)
-  }
+    private fun showMovieList(movies: List<Movie>) {
+        binding.movieRecyclerview.visibility = View.VISIBLE
+        binding.progressBar.visibility = View.GONE
+        binding.textview.visibility = View.GONE
+        movieAdapter.setCharacterList(movies)
+    }
 
-  private fun showErrorState() {
-    binding.movieRecyclerview.visibility = View.GONE
-    binding.progressBar.visibility = View.GONE
-    binding.textview.visibility = View.VISIBLE
-    binding.textview.text = getString(R.string.something_went_wrong)
-  }
+    private fun showErrorState() {
+        binding.movieRecyclerview.visibility = View.GONE
+        binding.progressBar.visibility = View.GONE
+        binding.textview.visibility = View.VISIBLE
+        binding.textview.text = getString(R.string.something_went_wrong)
+    }
 
-  override fun onDestroyView() {
-    super.onDestroyView()
-    _binding = null
-  }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
